75cb211d775c2454900fb2bfdfce19d2960c4397
kustovatatyana1994 <kustova.tatyana1994@yandex.ru>
2022-09-04 18:51:37 +0300

    usage TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemainLessBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainEquallyBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000
+                ;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainMoreBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+        }
+}
