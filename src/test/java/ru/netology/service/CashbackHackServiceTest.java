992ce5bb318d889bf00f2e7909e5e457c5958d62
kustovatatyana1994 <kustova.tatyana1994@yandex.ru>
2022-09-04 20:32:25 +0300

    usage junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,8 +25,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000
-                ;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
